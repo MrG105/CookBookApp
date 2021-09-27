738a0997f53d47289d45a65b296e53ee29d69457
Zi-on <zioniflores12@yahoo.com>
2021-09-26 20:34:20 -0500

    added login.js page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import Home from './pages/Home';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const client = new ApolloClient({
   uri: '/graphql',
@@ -10,9 +12,9 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      
+    <Header />
     <Home />
-
+    <Footer />
     </ApolloProvider>
   );
 }
