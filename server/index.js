beaa5fa761fe15309b42d80912bb4f2f1f95c44b
Gabriel Gutierrez <poo328@my.utsa.edu>
2021-09-28 22:28:26 -0500

    fixed graphql server proxy and something is going on with node_modules

--- server/index.js
+++ server/index.js
@@ -10,13 +10,30 @@ const { authMiddleware } = require('./utils/auth');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: authMiddleware
-});
+// old busted
 
-server.applyMiddleware({ app });
+// const server = new ApolloServer({
+//     typeDefs,
+//     resolvers,
+//     context: authMiddleware
+// });
+
+// server.applyMiddleware({ app });
+
+// new hotness, running async to await for middleware
+let apolloServer
+async function startServer() {
+
+  apolloServer = new ApolloServer({
+      typeDefs,
+      resolvers,
+      context: authMiddleware
+
+  });
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app });
+}
+startServer();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -25,14 +42,17 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
   }
   
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-  });
+
+  // not needed yet? was trying to hit index.html which doesent exist.  
+
+  // app.get('*', (req, res) => {
+  //   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  // });
   
   db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
-      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${apolloServer.graphqlPath}`);
     });
   });
   
