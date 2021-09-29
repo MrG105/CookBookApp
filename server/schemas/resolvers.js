269761cdb4f9c8da7e80370dc3d73f244fb77c30
Billy Dorman <b.d.dorman12@gmail.com>
2021-09-28 19:39:30 -0500

    create home.js, edit queries.js, edit resolvers.js, edit typdefs.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,27 +1,75 @@
-const {User, Recipe} = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { User, Recipe } = require('../models');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
-    Query: {
-        user: async() => {
-            return User.find({});
-        },
-        books: async(parent, {_id}) =>{
-            const params = _id? {_id} : {};
-            return Recipe.find(params);
-        },
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id })
+          .select('-__v -password')
+
+        return userData;
+      }
+
+      throw new AuthenticationError('Not logged in');
     },
-    Mutation: {
-        createRecipe: async (parent, args) => {
-            const recipe = await Recipe.create(args);
-            return recipe;
-        },
-
-       createUser: async (parent, args) => {
-           const user = await User.create(args);
-           return user;
-       }
-
+    users: async () => {
+      return User.find()
+        .select('-__v -password')
+    },
+    user: async (parent, { username }) => {
+      return User.findOne({ username })
+        .select('-__v -password')
+    },
+    recipes: async (parent, { username }) => {
+      const params = username ? { username } : {};
+      return Recipe.find(params).sort({ createdAt: -1 });
+    },
+    recipe: async (parent, { _id }) => {
+      return Recipe.findOne({ _id });
     }
+  },
+
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+    addRecipe: async (parent, args, context) => {
+      if (context.user) {
+        const recipe = await Recipe.create({ ...args, username: context.user.username });
+
+        await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $push: { recipe: recipe._id } },
+          { new: true }
+        );
+
+        return recipe;
+      }
+
+      throw new AuthenticationError('You need to be logged in!');
+    },
+  }
 };
 
 module.exports = resolvers;
