a21a39648957d911b40e298cc78ca9541a3ec121
Gabriel Gutierrez <poo328@my.utsa.edu>
2021-09-28 19:39:39 -0500

    added route for addrecipe and ingredients

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Login from './pages/Login';
 import signup from './pages/Signup';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import RecipeForm from './components/RecipeForm';
 
 const client = new ApolloClient({
   uri: '/graphql',
@@ -21,6 +22,7 @@ function App() {
           <Route exact path="/" component={Home} />
           <Route exact path="/login" component={Login} />
           {/* <Route exact path="/signup" component={Signup} /> */}
+          <Route exact path="/addRecipe" component={RecipeForm} />
           
         </Switch>
       </Router>
