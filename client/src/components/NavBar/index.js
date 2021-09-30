6f651ba0760e9a231afb1dd69ee426e4a39a1752
Zi-on <zioniflores12@yahoo.com>
2021-09-29 22:09:59 -0500

    got the sign up to save the password so login works now

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -1,25 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-
+import Auth from "../../utils/auth";
 function NavBar() {
-    return (
-        <nav className="navbar">
-            <ul className="flex-row">
-                <li className="mx-2">
-                    <Link to="/">Home</Link>
-                </li>
-                <li className="mx-2">
-                    <Link to="/recipeList">Recipe List</Link>
-                </li>
-                <li className="mx-2">
-                    <Link to="/addRecipe">New Recipe</Link>
-                </li>
-                <li className="mx-2">
-                    <Link to="/signup">Sign Up</Link>
-                </li>
-            </ul>
-        </nav>
-    )
+  const logout = (event) => {
+    event.preventDefault();
+    Auth.logout();
+  };
+
+  return (
+    <nav className="navbar">
+      <div>
+        <ul className="flex-row">
+          {Auth.loggedIn() ? (
+            <>
+              <li className="mx-2">
+                <Link to="/">Home</Link>
+              </li>
+              <li className="mx-2">
+                <Link to="/recipeList">Recipe List</Link>
+              </li>
+              <li className="mx-2">
+                <Link to="/addRecipe">New Recipe</Link>
+              </li>
+              <li className="mx-2" onClick={logout}>
+                Logout
+              </li>
+            </>
+          ) : (
+            <>
+              
+                <Link className="mx-2" to="/">Home</Link>
+        
+              
+                <Link className="mx-2" to="/login">
+                  Login
+                </Link>
+              
+                <Link className="mx2" to="/signup">
+                  Signup
+                </Link>
+             
+            </>
+          )}
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
 export default NavBar;
