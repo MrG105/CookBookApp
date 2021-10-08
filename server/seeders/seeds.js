8e7d6d59697ab1eee9d26e9e09a61e39e30b5520
Gabriel Gutierrez <poo328@my.utsa.edu>
2021-10-07 19:52:49 -0500

    added seeding

---
+++ server/seeders/seeds.js
@@ -0,0 +1,29 @@
+const db = require('../config/connection');
+const { User, Recipe } = require('../models');
+const userSeeds = require('./userSeeds.json');
+const recipeSeeds = require('./recipeSeeds.json');
+
+db.once('open', async () => {
+    try {
+      await Recipe.deleteMany({});
+      await User.deleteMany({});
+  
+      await User.create(userSeeds);
+  
+      for (let i = 0; i < recipeSeeds.length; i++) {
+        const recipe = await Recipe.create(recipeSeeds[i]);
+        const Author = recipe.author
+        const user = await User.findOneAndUpdate(
+          { username: Author },
+          { $push: { savedRecipes: recipe } },
+          
+        );
+      }
+    } catch (err) {
+      console.error(err);
+      process.exit(1);
+    }
+  
+    console.log('all done!');
+    process.exit(0);
+  });
